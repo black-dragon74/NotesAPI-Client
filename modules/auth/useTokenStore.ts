5402827fefdc7aedf2d660cb5c55ce412fd1f3b6
black.dragon74 <nickk.2974@gmail.com>
2021-05-06 23:20:40 +0530

    Use custom type for tokens
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- modules/auth/useTokenStore.ts
+++ modules/auth/useTokenStore.ts
@@ -1,8 +1,9 @@
 import create from "zustand"
 import { accessTokenKey, refreshTokenKey } from "../../lib/constants"
+import { TokenType } from "../../types/TokenType"
 
 export const useTokenStore = create(set => ({
-  setTokens: (x: { accessToken: string; refreshToken: string }) => {
+  setTokens: (x: TokenType) => {
     try {
       localStorage.setItem(accessTokenKey, x.accessToken)
       localStorage.setItem(refreshTokenKey, x.refreshToken)
