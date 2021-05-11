f9fa97db63ec8440ca160c12951cee84d1892acd
black.dragon74 <nickk.2974@gmail.com>
2021-05-11 20:39:56 +0530

    Add responsiveness hook
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

---
+++ hooks/useScreenType.ts
@@ -0,0 +1,22 @@
+import { useMediaQuery } from "react-responsive"
+
+export enum ScreenType {
+  OneCol,
+  TwoCol,
+  ThreeCol,
+  FullScreen,
+}
+
+const useScreenType = (): ScreenType => {
+  const is3Cols = useMediaQuery({ minWidth: 1336 })
+  const is2Cols = useMediaQuery({ minWidth: 1265 })
+  const is1Cols = useMediaQuery({ minWidth: 800 })
+
+  if (is3Cols) return ScreenType.ThreeCol //lg
+  if (is2Cols) return ScreenType.TwoCol //md
+  if (is1Cols) return ScreenType.OneCol //sm
+
+  return ScreenType.FullScreen //xs
+}
+
+export default useScreenType
