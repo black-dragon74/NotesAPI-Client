3e3814b2fc14e39eaf96235b8debffb8749c3f5d
black.dragon74 <nickk.2974@gmail.com>
2021-05-12 14:16:56 +0530

    Fix layout issues when using SSR
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,27 @@
 import "../styles/globals.scss"
 import { AppProps } from "next/app"
+import { isServer } from "../lib/isServer"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Render only on the client side if `getInitialProps` is not present
+  // FIXME: Can be avoided if we use `useScreenType` hook on server-side
+  if (isServer && !Component.getInitialProps) {
+    return null
+  }
+
   // TODO: Add viewport and touch-icons meta
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no, user-scalable=0"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
