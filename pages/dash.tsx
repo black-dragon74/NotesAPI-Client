25c924412db2669704ddf5d42bcfe4cca3f72600
black.dragon74 <nickk.2974@gmail.com>
2021-05-12 22:41:41 +0530

    Finish note card
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- pages/dash.tsx
+++ pages/dash.tsx
@@ -8,7 +8,16 @@ const Dash = () => {
     <DesktopLayout>
       <HeaderController title="Dashboard"></HeaderController>
       <MiddlePanel>
-        <NoteCard title="Henlo" />
+        <div className="flex flex-col gap-3">
+          {[1, 2, 3, 4, 5].map(n => (
+            <NoteCard
+              key={n}
+              title="Henlo"
+              excerpt="So this is really just a demo of the note and I hab no fucki'n idea what to do with it. Also, never try TS with React. You'll be fixing more than you would be creating."
+              onClick={() => console.log("Click kar diya gays!")}
+            />
+          ))}
+        </div>
       </MiddlePanel>
     </DesktopLayout>
   )
--- ui/NoteCard.tsx
+++ ui/NoteCard.tsx
@@ -17,7 +17,27 @@ const NoteCard: FC<NoteCardProps> = ({
       onClick={onClick}
       className="flex flex-col w-full p-4 rounded-lg transition duration-200 ease-in-out bg-primary-800 hover:bg-primary-700"
     >
-      Click krdo gays!
+      <div className="flex w-full justify-between space-x-4">
+        <div className="flex text-primary-100 items-center font-bold leading-5 truncate w-full">
+          <span className="inline truncate">{title}</span>
+        </div>
+        <div className="flex flex-shrink-0">
+          <div className="text-primary-600 font-bold items-center">Pinned</div>
+        </div>
+      </div>
+      <div className="w-full mt-2 flex">
+        <div className="text-primary-300 text-left w-full line-clamp-2">
+          <span>{excerpt}</span>
+        </div>
+      </div>
+      <div className="flex mt-2 w-full font-bold text-primary-600 justify-between">
+        <div>
+          <span className="inline">Created on: xx-xx-xx</span>
+        </div>
+        <div>
+          <span className="inline">Created by: Nick</span>
+        </div>
+      </div>
     </button>
   )
 }
