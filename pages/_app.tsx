c8823a992d8d09aa21ebef00e156c9c3c7bde409
black.dragon74 <nickk.2974@gmail.com>
2021-05-12 14:24:59 +0530

    Fix SSR in the proper way
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
 import "../styles/globals.scss"
 import { AppProps } from "next/app"
-import { isServer } from "../lib/isServer"
+import { Context as ResponsiveContext } from "react-responsive"
 import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // Render only on the client side if `getInitialProps` is not present
-  // FIXME: Can be avoided if we use `useScreenType` hook on server-side
-  if (isServer && !Component.getInitialProps) {
-    return null
-  }
-
-  // TODO: Add viewport and touch-icons meta
+  // TODO: Add touch-icons meta and manifests
   return (
     <>
       <Head>
@@ -19,7 +13,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, user-scalable=no, user-scalable=0"
         />
       </Head>
-      <Component {...pageProps} />
+      <ResponsiveContext.Provider value={{ width: 1336 }}>
+        <Component {...pageProps} />
+      </ResponsiveContext.Provider>
     </>
   )
 }
