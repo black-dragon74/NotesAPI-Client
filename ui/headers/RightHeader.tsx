61816645e6ed709d13b3d6695110cc5abebe5796
black.dragon74 <nickk.2974@gmail.com>
2021-05-16 00:37:39 +0530

    Added logout button
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- ui/headers/RightHeader.tsx
+++ ui/headers/RightHeader.tsx
@@ -1,7 +1,16 @@
+import { useTokenStore } from "../../modules/auth/useTokenStore"
+import Button from "../Button"
+
 const RightHeader = () => {
   return (
     <div className="flex space-x-4 justify-end items-center focus:outline-no-chrome w-full text-primary-100">
-      Profile
+      <Button
+        onClick={() => {
+          useTokenStore.getState().clearTokens()
+        }}
+      >
+        Logout
+      </Button>
     </div>
   )
 }
