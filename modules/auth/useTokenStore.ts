539e52002e0458f2907003f118ebbb9394b795cc
black.dragon74 <nickk.2974@gmail.com>
2021-05-23 16:06:23 +0530

    Make accessToken and refreshToken available for SSR
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- modules/auth/useTokenStore.ts
+++ modules/auth/useTokenStore.ts
@@ -3,6 +3,7 @@ import { combine } from "zustand/middleware"
 import { accessTokenKey, refreshTokenKey } from "../../lib/constants"
 import { isServer } from "../../lib/isServer"
 import { TokenType } from "../../types/TokenType"
+import Cookies from "js-cookie"
 
 const getDefaultValues = (): TokenType => {
   if (!isServer) {
@@ -24,16 +25,26 @@ export const useTokenStore = create(
   combine(getDefaultValues(), set => ({
     setTokens: (x: TokenType) => {
       try {
+        // Client side
         localStorage.setItem(accessTokenKey, x.accessToken)
         localStorage.setItem(refreshTokenKey, x.refreshToken)
+
+        // Server side
+        Cookies.set(accessTokenKey, x.accessToken)
+        Cookies.set(refreshTokenKey, x.refreshToken)
       } catch {}
       set(x)
     },
     clearTokens: () => {
       const tempToken: TokenType = { accessToken: "", refreshToken: "" }
       try {
+        // Client side
         localStorage.setItem(accessTokenKey, tempToken.accessToken)
         localStorage.setItem(refreshTokenKey, tempToken.refreshToken)
+
+        // Server side
+        Cookies.set(accessTokenKey, tempToken.accessToken)
+        Cookies.set(refreshTokenKey, tempToken.refreshToken)
       } catch {}
       set(tempToken)
     },
