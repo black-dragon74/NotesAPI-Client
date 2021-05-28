cc9e8083e2ca5f55a2b853b7ae3bc2378a64c24f
black.dragon74 <nickk.2974@gmail.com>
2021-05-29 03:03:14 +0530

    Refactor session refresh/validation process
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- hooks/useSessionRefresh.ts
+++ hooks/useSessionRefresh.ts
@@ -1,58 +1,85 @@
-import axios from "axios"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
+import { API_URL } from "../lib/constants"
 import { useTokenStore } from "../modules/auth/useTokenStore"
 import { TokenType } from "../types/TokenType"
 
+const validateURL = `${API_URL}/session/validate`
+const refreshURL = `${API_URL}/session/refresh`
+
+type ValidatorType = {
+  // Valid is returned when the success validation check is passed
+  // If validation is falsy, we try to refresh, if such is the case, we return `refreshed` and `token`
+  // Else, both things are invalid and we log the user out by returning `failed`
+  opcode: "valid" | "refreshed" | "failed"
+  token?: TokenType
+}
+
+const validate = async ({
+  accessToken,
+  refreshToken,
+}: TokenType): Promise<ValidatorType> => {
+  const validateResp = await fetch(validateURL, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+
+  // Session is valid, all systems go!
+  if (validateResp.ok) {
+    return { opcode: "valid" }
+  }
+
+  // Otherwise, maybe the token is expired?
+  const refreshResp = await fetch(refreshURL, {
+    method: "POST",
+    headers: {
+      refresh_token: refreshToken,
+    },
+  })
+
+  // Token refresh failed
+  if (!refreshResp.ok) {
+    return { opcode: "failed" }
+  }
+
+  // Otherwise get and return the data
+  const body = await refreshResp.json()
+  const token: TokenType = {
+    accessToken: body.data.authentication_token,
+    refreshToken: body.data.refresh_token,
+  }
+
+  return { opcode: "refreshed", token }
+}
+
 const useSessionRefresh = () => {
   const [connected, setConnected] = useState(false)
-  const { accessToken, refreshToken, setTokens } = useTokenStore.getState()
+  const { accessToken, refreshToken, setTokens, clearTokens } =
+    useTokenStore.getState()
   const { replace } = useRouter()
   const hasTokens = !!(accessToken && refreshToken)
 
   useEffect(() => {
     if (!connected && hasTokens) {
-      axios
-        .get("http://localhost:8030/v1/session/validate", {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/json",
-          },
-        })
-        .then(resp => {
-          if (resp.status === 200) {
+      validate({ accessToken, refreshToken }).then(v => {
+        switch (v.opcode) {
+          case "valid":
             setConnected(true)
-          }
-        })
-        .catch(() => {
-          axios
-            .post("http://localhost:8030/v1/session/refresh", null, {
-              headers: {
-                refresh_token: refreshToken,
-                "Content-Type": "application/json",
-              },
-            })
-            .then(resp => {
-              const tokenResp: TokenType = {
-                accessToken: resp.data.data.authentication_token,
-                refreshToken: resp.data.data.refresh_token,
-              }
-              if (tokenResp.accessToken && tokenResp.refreshToken) {
-                setConnected(true)
-                setTokens({
-                  accessToken: tokenResp.accessToken,
-                  refreshToken: tokenResp.refreshToken,
-                })
-              }
-            })
-            .catch(() => {
-              useTokenStore.getState().clearTokens()
-              replace("/logout")
-            })
-        })
+            break
+          case "failed":
+            clearTokens()
+            replace("/logout")
+            break
+          case "refreshed":
+            setTokens(v.token || ({} as TokenType))
+            setConnected(true)
+            break
+        }
+      })
     }
     // eslint-disable-next-line
-  }, [connected, hasTokens, replace])
+  }, [connected, accessToken, refreshToken, hasTokens, replace])
 
   return connected
 }
