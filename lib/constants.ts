137ee910e80f6f0306e936bf137a0e4864e160f4
black.dragon74 <nickk.2974@gmail.com>
2021-05-31 22:57:30 +0530

    Allow overrides using env
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
-export const NAME = "OnlyNotes"
+export const NAME = process.env.NEXT_PUBLIC_NAME || "Only Notes"
 
 export const refreshTokenKey = "refreshToken"
 export const accessTokenKey = "accessToken"
 export const nextPathKey = "loadNext"
 
-export const API_URL = "http://localhost:8030/v1"
+export const API_URL = process.env.API_URL || "http://localhost:8030/v1"
